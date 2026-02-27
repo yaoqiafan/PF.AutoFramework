76ed332f2e7829015680c4b517e67a432d5721fe
要恰饭的嘛 <1243411723@qq.com>
2026-02-27 13:22:19 +0800

    将工站抽象出来，主站继承抽象

---
+++ PF.Core/Events/PhysicalButtonEventBus.cs
@@ -0,0 +1,23 @@
+﻿using PF.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PF.Core.Events
+{
+    /// <summary>
+    /// 硬件事件总线的默认实现 (需在 DI 容器中注册为 Singleton)
+    /// </summary>
+    public class PhysicalButtonEventBus
+    {
+        public event Action<PhysicalButtonType> PhysicalButtonPressed;
+
+        public void PublishPhysicalButton(PhysicalButtonType buttonType)
+        {
+            // 线程安全地触发事件
+            PhysicalButtonPressed?.Invoke(buttonType);
+        }
+    }
+}
