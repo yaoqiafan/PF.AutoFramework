4a25a47d28665bdf68a24d04b6875cccce05243e
要恰饭的嘛 <1243411723@qq.com>
2026-03-05 15:13:01 +0800

    修改默认权限

--- PF.Modules.Identity/IdentityModule.cs
+++ PF.Modules.Identity/IdentityModule.cs
@@ -1,10 +1,7 @@
 ﻿using PF.Core.Constants;
-using PF.Modules.Identity.Helpers;
 using PF.Modules.Identity.ViewModels;
 using PF.Modules.Identity.Views;
 using PF.UI.Infrastructure.Navigation;
-using Prism.Ioc;
-using Prism.Modularity;
 using System.Reflection;
 
 namespace PF.Modules.Identity
@@ -16,8 +13,6 @@ namespace PF.Modules.Identity
             var navMenuService = containerProvider.Resolve<INavigationMenuService>();
             navMenuService.RegisterAssembly(Assembly.GetExecutingAssembly());
 
-            // 用所有已注册菜单的 Title 初始化 PermissionHelper 的动态中文名称映射
-            PermissionHelper.Initialize(navMenuService);
         }
 
         public void RegisterTypes(IContainerRegistry containerRegistry)
