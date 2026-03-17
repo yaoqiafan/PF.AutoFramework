04f84a6795c633cda7794045e2a6079b9874a645
Anson Gu <2604038287@qq.com>
2026-03-17 17:45:34 +0800

    添加光源控制器

---
+++ PF.Infrastructure/Hardware/LightController/BaseLightController.cs
@@ -0,0 +1,25 @@
+﻿using PF.Core.Interfaces.Device.Hardware;
+using PF.Core.Interfaces.Device.Hardware.Card;
+using PF.Core.Interfaces.Device.Hardware.LightController;
+using PF.Core.Interfaces.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PF.Infrastructure.Hardware.LightController
+{
+    public abstract class BaseLightController : BaseDevice, ILightController
+    {
+        protected BaseLightController(string deviceId, string deviceName, bool isSimulated, ILogService logger) : base(deviceId, deviceName, isSimulated, logger)
+        {
+            Category = Core.Enums.HardwareCategory.LightController ;
+        }
+
+       
+
+    
+        public abstract Task SetLightValue(int Channel, int LightValue);
+    }
+}
---
+++ PF.Infrastructure/Hardware/LightController/OPT/OPTLightController.cs
@@ -0,0 +1,36 @@
+﻿using PF.Core.Interfaces.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PF.Infrastructure.Hardware.LightController.OPT
+{
+    internal class OPTLightController : BaseLightController
+    {
+        public OPTLightController(string deviceId, string deviceName, bool isSimulated, ILogService logger) : base(deviceId, deviceName, isSimulated, logger)
+        {
+        }
+
+        public override Task SetLightValue(int Channel, int LightValue)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Task<bool> InternalConnectAsync(CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Task InternalDisconnectAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override Task InternalResetAsync(CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
