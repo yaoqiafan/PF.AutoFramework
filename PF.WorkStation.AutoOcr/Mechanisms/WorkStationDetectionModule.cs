a2036646fda1fb4650d1d2fe77968f54669d82b5
Anson Gu <2604038287@qq.com>
2026-03-20 11:24:57 +0800

    模组传入添加参数

--- PF.WorkStation.AutoOcr/Mechanisms/WorkStationDetectionModule.cs
+++ PF.WorkStation.AutoOcr/Mechanisms/WorkStationDetectionModule.cs
@@ -1,4 +1,6 @@
-﻿using PF.Core.Attributes;
+﻿using log4net.Repository.Hierarchy;
+using PF.Core.Attributes;
+using PF.Core.Interfaces.Configuration;
 using PF.Core.Interfaces.Device.Hardware;
 using PF.Core.Interfaces.Device.Hardware.IO.Basic;
 using PF.Core.Interfaces.Device.Hardware.Motor.Basic;
@@ -22,15 +24,15 @@ namespace PF.WorkStation.AutoOcr.Mechanisms
     public class WorkStationDetectionModule : BaseMechanism
     {
 
-        private  IAxis XAxis;
+        private IAxis XAxis;
 
-        private  IAxis YAxis;
+        private IAxis YAxis;
 
-        private  IAxis ZAxis;
-       
-        public WorkStationDetectionModule(IHardwareManagerService hardwareManagerService, ILogService logger) : base(E_Mechanisms.OCR识别模组.ToString(), hardwareManagerService, logger)
+        private IAxis ZAxis;
+
+        public WorkStationDetectionModule(IHardwareManagerService hardwareManagerService, IParamService paramService, ILogService logger) : base(E_Mechanisms.OCR识别模组.ToString(), hardwareManagerService, paramService, logger)
         {
-           
+
         }
 
         protected override async Task<bool> InternalInitializeAsync(CancellationToken token)
@@ -39,31 +41,62 @@ namespace PF.WorkStation.AutoOcr.Mechanisms
             XAxis = HardwareManagerService?.GetDevice(E_AxisName.视觉X轴.ToString()) as IAxis;
             if (XAxis == null)
             {
+                _logger.ShowUiMessage($"{E_AxisName.视觉X轴}未注册", Core.Enums.LogLevel.Error);
                 return false;
             }
             YAxis = HardwareManagerService.GetDevice(E_AxisName.视觉Y轴.ToString()) as IAxis;
+            if (YAxis == null)
+            {
+                _logger.ShowUiMessage($"{E_AxisName.视觉Y轴}未注册", Core.Enums.LogLevel.Error);
+                return false;
+            }
             ZAxis = HardwareManagerService.GetDevice(E_AxisName.视觉Z轴.ToString()) as IAxis;
+            if (ZAxis == null)
+            {
+                _logger.ShowUiMessage($"{E_AxisName.视觉Z轴}未注册", Core.Enums.LogLevel.Error);
+                return false;
+            }
+            RegisterHardwareDevice(XAxis as IHardwareDevice);
+            RegisterHardwareDevice(YAxis as IHardwareDevice);
+            RegisterHardwareDevice(ZAxis as IHardwareDevice);
+
             //电机使能
             if (!await XAxis.EnableAsync(token))
             {
+                _logger.ShowUiMessage($"{E_AxisName.视觉X轴}使能失败", Core.Enums.LogLevel.Error);
                 return false;
             }
 
             if (!await YAxis.EnableAsync(token))
             {
+                _logger.ShowUiMessage($"{E_AxisName.视觉Y轴}使能失败", Core.Enums.LogLevel.Error);
                 return false;
             }
 
             if (!await ZAxis.EnableAsync(token))
             {
+                _logger.ShowUiMessage($"{E_AxisName.视觉Z轴}使能失败", Core.Enums.LogLevel.Error);
                 return false;
             }
             if (!await ZAxis.HomeAsync(token))
             {
+                _logger.ShowUiMessage($"{E_AxisName.视觉Z轴}回零失败", Core.Enums.LogLevel.Error);
                 return false;
             }
 
+            if (!await WaitHomeDone(ZAxis, token))
+            {
+                return false;
+            }
 
+            if (!await XAxis.HomeAsync(token) || !await YAxis.HomeAsync(token))
+            {
+                return false;
+            }
+            if (!await WaitHomeDone(XAxis, token) || !await WaitHomeDone(YAxis, token))
+            {
+                return false;
+            }
             return true;
         }
 
@@ -83,9 +116,18 @@ namespace PF.WorkStation.AutoOcr.Mechanisms
                         }
                     }
                 });
+                Task b = Task.Delay(await ParamService.GetParamAsync<int>(E_TimeOut.电机回零超时.ToString()), token);
+                Task result = await Task.WhenAny(a, b);
+                if (result.Equals(a))
+                {
+                    return true;
+                }
+                await axis.StopAsync(token);
+                return false;
             }
             catch (Exception ex)
             {
+                await axis.StopAsync(token);
                 return false;
             }
         }
