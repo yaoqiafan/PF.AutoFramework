a0e3eff792687d26fb4405fbe11ab565e361a12b
要恰饭的嘛 <1243411723@qq.com>
2026-03-23 13:40:38 +0800

    fix: 修复主控报警死锁问题 + UI更新 + AutoOCR工站框架
    - BaseMasterController: Task.Run包装Fire(Error)，切断同步调用链
    - StationBase: Alarm状态判断，防止重入
    - MainWindow/Splash/DebugView: UI调整
    - 新增AutoOCRMachineController、WorkStation1FeedingStation、WorkstationSignals

---
+++ PF.WorkStation.AutoOcr/Stations/AutoOCRMachineController.cs
@@ -0,0 +1,20 @@
+﻿using PF.Core.Events;
+using PF.Core.Interfaces.Logging;
+using PF.Infrastructure.Station;
+using PF.Infrastructure.Station.Basic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PF.WorkStation.AutoOcr.Stations
+{
+    public class AutoOCRMachineController : BaseMasterController
+    {
+        public AutoOCRMachineController(ILogService logger, PhysicalButtonEventBus hardwareEventBus, IEnumerable<StationBase> subStations) : base(logger, hardwareEventBus, subStations)
+        {
+
+        }
+    }
+}
