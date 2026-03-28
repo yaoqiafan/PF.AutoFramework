82a1fe36d4eaf5f2525319948ee144325c040da8
要恰饭的嘛 <1243411723@qq.com>
2026-03-28 19:40:09 +0800

    修改点击规则

--- PF.Modules.SecsGem/Views/SecsGemDebugView.xaml.cs
+++ PF.Modules.SecsGem/Views/SecsGemDebugView.xaml.cs
@@ -5,6 +5,8 @@ using System;
 using System.Collections.Specialized;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
 
 namespace PF.Modules.SecsGem.Views
 {
@@ -52,6 +54,52 @@ namespace PF.Modules.SecsGem.Views
                 vm.SelectCommandLeafCommand.Execute(leaf);
                 e.Handled = true;
             }
+        }
+
+
+
+        /// <summary>
+        /// 树视图鼠标右键按下事件处理程序
+        /// 当在TreeView上按下鼠标右键时，查找并选中鼠标位置下的TreeViewItem
+        /// 这样可以确保右键点击时正确的TreeViewItem被选中，以便显示正确的上下文菜单
+        /// </summary>
+        /// <param name="sender">事件源，TreeView控件</param>
+        /// <param name="e">鼠标事件参数，包含鼠标位置、按钮状态等信息</param>
+        private void TreeView_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            // 查找鼠标位置下的TreeViewItem
+            TreeViewItem? treeViewItem = FindAncestor<TreeViewItem>((DependencyObject)e.OriginalSource);
+
+            if (treeViewItem != null)
+            {
+                // 将找到的TreeViewItem设置为选中状态
+                treeViewItem.IsSelected = true;
+
+                // 标记事件已处理，防止事件继续冒泡
+                e.Handled = true;
+            }
+        }
+
+        /// <summary>
+        /// 在可视化树中查找指定类型的祖先元素
+        /// 通过遍历可视化树向上查找，直到找到指定类型的元素或到达根元素
+        /// </summary>
+        /// <typeparam name="T">要查找的依赖对象类型</typeparam>
+        /// <param name="current">起始的依赖对象，通常是事件源</param>
+        /// <returns>找到的指定类型的祖先元素，如果未找到则返回null</returns>
+        private T? FindAncestor<T>(DependencyObject current) where T : DependencyObject
+        {
+            // 循环向上遍历可视化树
+            while (current != null)
+            {
+                // 检查当前元素是否为指定类型
+                if (current is T ancestor)
+                    return ancestor;
+
+                // 获取当前元素的可视化父元素，继续向上查找
+                current = VisualTreeHelper.GetParent(current);
+            }
+            return null;
         }
     }
 }
