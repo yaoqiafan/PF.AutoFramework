bde88f8baf675721fc861f350a43c7db75fc2b41
要恰饭的嘛 <1243411723@qq.com>
2026-04-09 15:17:21 +0800

    添加轴参数个更改对话框

--- PF.Modules.Debug/DebugModule.cs
+++ PF.Modules.Debug/DebugModule.cs
@@ -1,4 +1,5 @@
 ﻿using PF.Core.Constants;
+using PF.Modules.Debug.Dialogs;
 using PF.Modules.Debug.ViewModels;
 using PF.Modules.Debug.Views;
 using PF.UI.Infrastructure.Navigation;
@@ -36,6 +37,8 @@ namespace PF.Modules.Debug
 
             containerRegistry.RegisterForNavigation<BarcodeScanDebugView, BarcodeScanDebugViewModel>(NavigationConstants.Views.BarcodeScanDebugView);
 
+
+
             // 3. 注册工站调试容器视图（子工站调试视图由各工站 UI 模块自行注册）
             containerRegistry.RegisterForNavigation<StationDebugView, StationDebugViewModel>(NavigationConstants.Views.StationDebugView);
 
@@ -43,6 +46,9 @@ namespace PF.Modules.Debug
             containerRegistry.RegisterForNavigation<CardDebugView, CardDebugViewModel>(NavigationConstants.Views.CardDebugView);
 
             containerRegistry.RegisterForNavigation<CameraDebugView, CameraDebugViewModel>(NavigationConstants.Views.CameraDebugView);
+
+
+            containerRegistry.RegisterDialog<AxisParamDialog, AxisParamDialogViewModel>(nameof(AxisParamDialog));
         }
     }
 }
