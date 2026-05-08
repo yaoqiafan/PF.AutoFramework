06c36e87b546eea333106bba086cee74778f6b24
要恰饭的嘛 <1243411723@qq.com>
2026-05-08 17:39:18 +0800

    添加调试界面迷你模式

--- PF.WorkStation.AutoOcr.UI/UserControls/RecipeDebugView.xaml.cs
+++ PF.WorkStation.AutoOcr.UI/UserControls/RecipeDebugView.xaml.cs
@@ -1,13 +1,42 @@
+using System.Windows;
 using System.Windows.Controls;
 
 namespace PF.WorkStation.AutoOcr.UI.UserControls
 {
     public partial class RecipeDebugView : UserControl
     {
-        ///¹¹Ôìº¯Êý
+        private MiniDebugWindow? _miniWindow;
+
         public RecipeDebugView()
         {
             InitializeComponent();
+        }
+
+        private void BtnMiniMode_Click(object sender, RoutedEventArgs e)
+        {
+            if (_miniWindow?.IsVisible == true)
+            {
+                _miniWindow.Activate();
+                return;
+            }
+
+            var dialogWindow = Window.GetWindow(this);
+            var mainWindow = Application.Current.MainWindow;
+
+            _miniWindow = new MiniDebugWindow { DataContext = DataContext };
+            _miniWindow.OnClosed = () =>
+            {
+                dialogWindow?.Show();
+                if (mainWindow != null)
+                    mainWindow.WindowState = WindowState.Normal;
+                dialogWindow?.Activate();
+            };
+
+            dialogWindow?.Hide();
+            if (mainWindow != null && mainWindow != dialogWindow)
+                mainWindow.WindowState = WindowState.Minimized;
+
+            _miniWindow.Show();
         }
     }
 }
