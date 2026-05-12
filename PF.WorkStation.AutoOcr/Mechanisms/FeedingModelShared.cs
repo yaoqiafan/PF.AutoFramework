fbb8680ea479ce503a20080c2a25ea513ffb471f
要恰饭的嘛 <1243411723@qq.com>
2026-05-12 15:43:16 +0800

    添加寻层互斥

---
+++ PF.WorkStation.AutoOcr/Mechanisms/FeedingModelShared.cs
@@ -0,0 +1,11 @@
+﻿namespace PF.WorkStation.AutoOcr.Mechanisms
+{
+    /// <summary>
+    /// 寻层扫描互斥锁：确保工位1和工位2的 SearchLayerAsync 不会同时运行，
+    /// 避免两台 Z 轴同时执行锁存扫描时因共享运动控制卡资源产生干扰。
+    /// </summary>
+    internal static class FeedingModelShared
+    {
+        internal static readonly SemaphoreSlim SearchLayerLock = new SemaphoreSlim(1, 1);
+    }
+}
